5047013c2ce25c57b8012370d91514530f878f45
poxstone <poxstone@gmail.com>
2015-07-20 22:43:52 -0500

    add jsonp and json

--- expss/routes/index.js
+++ expss/routes/index.js
@@ -6,12 +6,32 @@ router.get('/', function(req, res, next) {
 	res.render('index', { title: 'Express', body_css : 'home' });
 });
 
-router.get('/lola', function(req, res, next) {
+router.get(/lola?/, function(req, res, next) {
 	res.render('lola', { title: 'perro lola', body_css : 'interna' });
 });
 
 router.post('/lola', function(req, res, next) {
 	res.render('lola', { title: 'perro lola', body_css : 'interna', posts: req.body });
+});
+
+router.post('/json', function(req, res, next) {
+	res.json({
+		name : 'oscar',
+		email : 'poxstone@gmail.com',
+		work : 'eforcers',
+		week : '5',
+		number : '7',
+	});
+});
+
+router.post('/jsonp', function(req, res, next) {
+	res.json({
+		name : 'oscar',
+		email : 'poxstone@gmail.com',
+		work : 'nuberti',
+		week : '4',
+		number : '8',
+	});
 });
 
 module.exports = router;
